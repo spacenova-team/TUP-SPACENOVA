f5017ddcf62aea44d18768e9b4f0128722c2e1bd
Lautaro Cardenas <cardenaslautaro8@gmail.com>
2026-06-23 15:00:45 -0300

    fix lighthouse

--- spacenova-front/lighthouserc.js
+++ spacenova-front/lighthouserc.js
@@ -4,6 +4,12 @@ module.exports = {
       staticDistDir: './dist/spacenova/browser',
       numberOfRuns: 3
     },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.5 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+      }
+    },
     upload: {
       target: 'filesystem',
       outputDir: './.lighthouseci'
