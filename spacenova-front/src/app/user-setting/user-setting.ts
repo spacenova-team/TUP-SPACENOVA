bd1c9c056a46531c2ee9b399e50a54b052b1776f
maitereynoso <maitereynosov@gmail.com>
2026-07-14 08:48:34 -0300

    fix sidebar to make it visible

--- spacenova-front/src/app/user-setting/user-setting.ts
+++ spacenova-front/src/app/user-setting/user-setting.ts
@@ -5,12 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { TranslatePipe } from '@ngx-translate/core';
-import { Header } from '../header/header';
 import { AnalyticsService } from '../analytics-service';
 
 @Component({
   selector: 'app-user-setting',
-  imports: [Sidenav, MatButtonModule, MatIconModule, MatDialogModule, TranslatePipe, Header],
+  imports: [Sidenav, MatButtonModule, MatIconModule, MatDialogModule, TranslatePipe],
   templateUrl: './user-setting.html',
   styleUrl: './user-setting.css'
 })
