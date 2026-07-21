a7318fe6d56154b254a223ed11dd837151e9ce07
maitereynoso <maitereynosov@gmail.com>
2026-07-21 19:05:16 -0300

    add role access

--- spacenova-back/firebase.js
+++ spacenova-back/firebase.js
@@ -1,11 +1,13 @@
 import { initializeApp, cert } from "firebase-admin/app"
 import { getFirestore } from "firebase-admin/firestore"
 import serviceAccount from "./spacenova-fb63f-firebase-adminsdk-fbsvc-ec56f80eca.json" with { type: "json" }
+import { getAuth } from "firebase-admin/auth"
 
 initializeApp({
     credential: cert(serviceAccount)
 })
 
 const db = getFirestore()
+const auth = getAuth()
 
 export { db }
